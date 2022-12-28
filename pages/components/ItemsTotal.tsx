2d136c31f363be727604ba00db525e5c3623f925
David Randall <david.t.randall@gmail.com>
2022-12-27 21:21:02 -0500

    items total static fix

--- pages/components/ItemsTotal.tsx
+++ pages/components/ItemsTotal.tsx
@@ -6,7 +6,9 @@ type Props = {
 };
 
 const ItemsTotal = ({ items }: Props) => {
-  const total = items.reduce((sum, current) => sum + current.count, 0);
+  const total = items
+    ? items.reduce((sum, current) => sum + current.count, 0)
+    : 0;
 
   return <div className="text-8xl text-slate-200 ">{total}</div>;
 };
